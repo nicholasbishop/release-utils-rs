e244a39aa250f26ae0c97cbfc7826eef47815174
Nicholas Bishop <nbishop@nbishop.net>
2024-01-14 11:52:04 -0500

    Add test for get_commit_message_subject

--- tests/integration/git.rs
+++ tests/integration/git.rs
@@ -25,3 +25,45 @@ fn test_repo_open() {
 
     assert!(Repo::open_path(tmp_dir.path()).is_ok());
 }
+
+#[test]
+fn test_get_commit_message_subject() {
+    let tmp_dir = TempDir::new().unwrap();
+
+    // Create a temporary repo.
+    let mut cmd = Command::new("git");
+    cmd.arg("init");
+    cmd.arg(tmp_dir.path());
+    run_cmd(cmd).unwrap();
+
+    // Configure identity.
+    let mut cmd = Command::new("git");
+    cmd.arg("-C");
+    cmd.arg(tmp_dir.path());
+    cmd.args(["config", "user.email", "release-utils-test@example.com"]);
+    run_cmd(cmd).unwrap();
+    let mut cmd = Command::new("git");
+    cmd.arg("-C");
+    cmd.arg(tmp_dir.path());
+    cmd.args(["config", "user.name", "Release Utils Test"]);
+    run_cmd(cmd).unwrap();
+
+    // Create an empty commit with a known commit message.
+    let mut cmd = Command::new("git");
+    cmd.arg("-C");
+    cmd.arg(tmp_dir.path());
+    cmd.args([
+        "commit",
+        "--allow-empty",
+        "-m",
+        "hello world!\n\nHere's the body.",
+    ]);
+    run_cmd(cmd).unwrap();
+
+    let repo = Repo::open_path(tmp_dir.path()).unwrap();
+
+    assert_eq!(
+        repo.get_commit_message_subject("HEAD").unwrap(),
+        "hello world!"
+    );
+}
