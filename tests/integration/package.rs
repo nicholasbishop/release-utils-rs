e77a1b232b1edaef4a5d1373d143dc42344f5a21
Nicholas Bishop <nbishop@nbishop.net>
2024-01-14 14:46:52 -0500

    Add test for get_local_version

--- tests/integration/package.rs
+++ tests/integration/package.rs
@@ -6,8 +6,11 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use release_utils::cmd::*;
 use release_utils::Package;
 use std::env;
+use std::process::Command;
+use tempfile::TempDir;
 
 #[test]
 fn test_package() {
@@ -15,4 +18,16 @@ fn test_package() {
     assert_eq!(pkg.name(), "foo");
     assert_eq!(pkg.workspace(), env::current_dir().unwrap());
     assert_eq!(pkg.get_git_tag_name("1.2.3"), "foo-v1.2.3");
+}
+
+#[test]
+fn test_package_local_version() {
+    let tmp_dir = TempDir::new().unwrap();
+    let mut cmd = Command::new("cargo");
+    cmd.args(["init", "--name", "foo"]);
+    cmd.arg(tmp_dir.path());
+    run_cmd(cmd).unwrap();
+
+    let pkg = Package::with_workspace("foo", tmp_dir.path());
+    assert_eq!(pkg.get_local_version().unwrap(), "0.1.0");
 }
