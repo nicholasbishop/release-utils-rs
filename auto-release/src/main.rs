6de1189529e16b29c701f1f72ae43d6e2ae5190c
Nicholas Bishop <nbishop@nbishop.net>
2024-01-20 14:12:37 -0500

    auto-release: Add check_condition

--- auto-release/src/main.rs
+++ auto-release/src/main.rs
@@ -6,7 +6,17 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use clap::Parser;
+use anyhow::Result;
+use clap::{Parser, ValueEnum};
+use release_utils::release::get_github_sha;
+use release_utils::Repo;
+use std::process;
+
+#[derive(ValueEnum, Clone, Copy)]
+enum Condition {
+    Body,
+    Subject,
+}
 
 #[derive(Parser)]
 struct Cli {
@@ -14,15 +24,57 @@ struct Cli {
     package: Vec<String>,
 
     #[arg(long)]
+    condition: Option<Condition>,
+
+    #[arg(long)]
     execute: bool,
 }
 
-fn main() {
+fn check_condition(condition: Condition) -> Result<bool> {
+    let commit_sha = get_github_sha()?;
+    let repo = Repo::open()?;
+
+    let prefix = "release:";
+
+    let msg_text;
+    let msg_kind;
+
+    match condition {
+        Condition::Body => {
+            msg_text = repo.get_commit_message_body(&commit_sha)?;
+            msg_kind = "body";
+        }
+        Condition::Subject => {
+            msg_text = repo.get_commit_message_subject(&commit_sha)?;
+            msg_kind = "subject";
+        }
+    }
+
+    if msg_text.starts_with(prefix) {
+        Ok(true)
+    } else {
+        println!("commit message {msg_kind} of {commit_sha} does not start with \"{prefix}\"");
+        Ok(false)
+    }
+}
+
+fn execute(cli: Cli) -> Result<()> {
+    if let Some(condition) = cli.condition {
+        if !check_condition(condition)? {
+            return Ok(());
+        }
+    }
+
+    todo!()
+}
+
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     if cli.execute {
-        todo!();
+        execute(cli)
     } else {
         println!("--execute not passed; stopping");
+        process::exit(1);
     }
 }
