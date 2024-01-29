071613cb6e00116efe671652a6acf8ddd5be98a5
Nicholas Bishop <nbishop@nbishop.net>
2024-01-29 01:37:37 -0500

    release-utils: Add get_cmd_stdout_utf8

--- release-utils/src/github.rs
+++ release-utils/src/github.rs
@@ -68,6 +68,7 @@ impl Gh {
         match run_cmd(cmd) {
             Ok(()) => Ok(true),
             Err(err @ RunCommandError::Launch { .. }) => Err(err),
+            Err(err @ RunCommandError::NonUtf8 { .. }) => Err(err),
             Err(RunCommandError::NonZeroExit { cmd, status }) => {
                 // There are probably other ways this could fail, but
                 // checking for code 1 should be close enough.
